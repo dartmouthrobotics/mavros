ff34ece5c2e3cd32add3e78ba0f05c8281edf0b2
slensgra <sam.lensgraf@gmail.com>
2019-11-08 09:40:57 -0500

    add more channels to the message type

--- mavros/src/plugins/rc_io.cpp
+++ mavros/src/plugins/rc_io.cpp
@@ -236,7 +236,7 @@ private:
 		ovr.target_component = m_uas->get_tgt_component();
 
 		// [[[cog:
-		// for i in range(1, 9):
+		// for i in range(1, 12):
 		//     cog.outl("ovr.chan%d_raw = req->channels[%d];" % (i, i - 1))
 		// ]]]
 		ovr.chan1_raw = req->channels[0];
@@ -247,6 +247,10 @@ private:
 		ovr.chan6_raw = req->channels[5];
 		ovr.chan7_raw = req->channels[6];
 		ovr.chan8_raw = req->channels[7];
+        ovr.chan9_raw = req->channels[8];
+        ovr.chan10_raw = req->channels[9];
+        ovr.chan11_raw = req->channels[10];
+        ovr.chan12_raw = req->channels[11];
 		// [[[end]]] (checksum: bd27f3e85f5ab614ce1332ae3f4c6ebd)
 
 		UAS_FCU(m_uas)->send_message_ignore_drop(ovr);
